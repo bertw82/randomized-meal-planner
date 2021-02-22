876b62da34bd2c05c6ddd5577d9b6f4723e95f18
Bert Witzel <bwitzel82@gmail.com>
2021-02-22 14:08:25 -0500

    Finishing touches
    Add a couple more meals, provide code commentary, tighten up styling

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const tagline = document.querySelector('.tagline');
 const startButton = document.getElementById('startButton');
 const planDiv = document.querySelector('.plan');
 const mealList = document.getElementById('meals');
-const myName = `Bert Witzel`; 
+const myName = `Bert`; 
 const myModal = document.getElementById('myModal');
+
+// meals from wikibooks.org
 const meals = [
     {
         name: 'Tuna Casserole',
@@ -154,10 +156,65 @@ const meals = [
             'Remove the quiche from the oven and let it cool for at least 20 minutes before serving'
         ],
         index: 6
+    },
+    {
+        name: 'Chicken Alfredo',
+        ingredients: [
+            '1 1/2 cups shredded cooked chicken, reheated',
+            '2 cups uncooked farfalle pasta',
+            '1 1/2 oz shredded Parmiggiano-Reggiano cheese (not the stuff in the green can)',
+            '1/4 cup unsalted butter, divided',
+            '1/4 cup heavy cream',
+            '4 oz (115 g) grated mozzarella cheese',
+            '1/2 tsp minced garlic',
+            '1 1/2 tbs all-purpose flour'
+        ],
+        instructions: [
+            'In a gallon (4 liters) of boiling salted water, cook pasta to al dente. Drain and rinse with cold water. Toss with chicken and set aside.',
+            'Melt 2 tbs butter in a medium saucepan over medium high heat. Once bubbling ceases, add flour and cook, whisking continuously until mixture turns light blonde in color.',
+            'Add remaining sauce ingredients. Bring to a simmer and reduce heat to low. Drizzle over pasta mixture and serve warm.'
+        ],
+        index: 7
+    },
+    {
+        name: 'Fish Pie',
+        ingredients: [
+            '200 g of fresh salmon',
+            '200 g of smoked haddock',
+            '100 g prawns',
+            '100 g mussels',
+            '5 scallops',
+            '2 lb (1 kg) potatoes',
+            'nutmeg',
+            'butter',
+            '1/4 Pt (150 ml) fresh single cream',
+            '2 bay leaves',
+            'milk',
+            '50 g Ementhaler or Appenzeller cheese, grated.'
+        ],
+        instructions: [
+            'Take all the fish and place it all in a saucepan with sufficient milk so that the fish is submerged.',
+            'Add the bay leaf and a pinch of salt',
+            'Poach on a low heat for about 5 minutes',
+            'Remove the fish from the pan, skin the haddock and the salmon, and flake, using a fork, into pieces.',
+            'Return to the milk and set aside.',
+            'Peel and boil the potatoes until they are soft enough to mash. This will take at least 20 minutes of boiling, otherwise the mash may remain lumpy (and who wants lumpy mash !!??)',
+            'The potato needs to be mashed very well indeed, and for this I personally recommend an electric whisk after the initial crushing of the potatoes.',
+            'Pour some of the milk into the potatoes so that the taste of fish infuses into it.',
+            'The mash should not get too runny, but add some of the cream and grate some nutmeg into the potato as well.',
+            'Now put the cooked fish into the bottom of an oven-proof dish, with any remaining milk sauce. The dish should not be too large as it is nice to build a couple of layers of the fish, rather than lay it out in one layer. Once you have done this, pat the fish gently down a little so that it is slightly compacted.',
+            'Now spread the mashed potato over the top of the fish to a height of about 1½ inches (38mm) making sure it is flat, and not piled up in the middle. Using a fork, ridge the top of the potato as this will make it colour up nicely in the oven.',
+            'At this stage, if you want, add the grated cheese over the potato, and if you are feeling artistic, decorate the top of the pie with some large peeled prawns around the edge of the fish, and maybe a decoration in the middle.',
+            'Place the pie in the oven at 220°C and leave for about 30 minutes or so, until the top of the potato/grated cheese is nicely coloured and the potato is bubbling nicely.',
+            'Remove from the oven and serve immediately.'
+        ],
+        index: 8
     }
 ]; 
 
 // HELPER FUNCTIONS
+
+// get current day of the week
 const getDay = () => {
     const today = new Date;
     const day = today.getDay();
@@ -165,11 +222,13 @@ const getDay = () => {
     return dayLong[day];
 };
 
+// function to get random meal from meals array
 const getRandomWeeklyMeals = () => {
     const randomMeal = meals[Math.floor(Math.random() * meals.length)];
     return randomMeal;
 };
 
+// function to create weekly meal plan
 const displayWeeklyMealPlan = () => {
     for (let i = 0; i < 7; i++) {
         const meal = getRandomWeeklyMeals();
@@ -181,17 +240,19 @@ const displayWeeklyMealPlan = () => {
     } 
 };
 
+// show weekly meal plan
 const showPlan = () => {
     displayWeeklyMealPlan();
     headline.classList.add('slideUp');
     headline.style.marginBottom = '0';
     headline.style.fontSize = '3em';
-    tagline.textContent = `Here's your weekly meal plan!`;
+    tagline.textContent = `Here's your weekly meal plan! Click on a meal to get the recipe`;
     tagline.style.marginTop = '0';
     planDiv.classList.remove('hidden');
     startButton.style.display = 'none';
 };
 
+// create modal window
 const createModal = (index) => {
     const recipe = meals[index];
     const h1 = document.createElement('h1');
@@ -221,12 +282,16 @@ const createModal = (index) => {
     myModal.classList.remove('hidden');
 }
 
+// opening page display
 headline.textContent = getDay();
 tagline.textContent = `Hey ${myName}, click the button below to see your weekly meal planner.`;
 
 // EVENT LISTENERS
+
+// show weekly meal plan
 startButton.addEventListener('click', showPlan);
 
+// open modal window when clicked
 mealList.addEventListener('click', e => {
     if (e.target !== mealList) {
         const recipe = e.target.closest(".meal-item");
@@ -235,9 +300,12 @@ mealList.addEventListener('click', e => {
     }
 });
 
+// close modal window
 myModal.addEventListener('click', e => {
     const closeButton = document.querySelector('.close');
     if (e.target === closeButton) {
+        const modalContent = document.querySelector('.modal-content');
         myModal.classList.add('hidden');
+        modalContent.remove();
     }
 });
